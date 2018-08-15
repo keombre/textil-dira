d9ddf843cad0eab4e1ccc23e53dce41162f8dc93
keombre <keombre8@gmail.com>
2018-08-16 01:36:05 +0200

    initial

---
+++ data.js
@@ -0,0 +1,42 @@
+racks = ["Midgard", "Jotunehim", "Asgard"]
+
+ether = {
+    "0ea": {
+        "loc": [0, 2, 15],
+        "dev": "printer",
+        "attrib": "01 2560"
+    },
+    "0eb": {
+        "loc": [0, 2, 15],
+        "dev": "switch",
+        "attrib": "TP-Link"
+    },
+    "0ec": {
+        "loc": [0, 2, 15],
+        "dev": "server"
+    },
+    "0ed": {
+        "loc": [0, 2, 15],
+        "dev": "computer",
+        "attrib": "01 2560"
+    },
+    "0ee": {
+        "loc": [0, 2, 15],
+        "dev": "ap"
+    },
+    "0ef": {
+        "loc": [0, 2, 15],
+        "dev": "empty"
+    },
+}
+
+tel = {
+    "0eg": {
+        "line": "25",
+        "dev": "phone"
+    },
+    "0eh": {
+        "line": "26",
+        "dev": "empty"
+    }
+}
---
+++ script.js
@@ -0,0 +1,72 @@
+function search(val) {
+    beforeSearch()
+    if (val.length > 4) notFound();
+
+    if (val.length == 3 && !isNaN(val.substring(0,1))) {
+        if (val.toLowerCase() in ether) {
+            displayEther(val);
+        } else {notFound()}
+    } else if (val.length == 2 && !isNaN(val)) {
+        let port = Object.keys(tel).find(key => tel[key].line === val)
+        if (port) {
+            displayTel(port)
+        } else {notFound()}
+    } else if (val.length == 4) {
+        if (val.substring(0,1).toLowerCase() == 't') {
+            let line = val.substring(1).toLowerCase()
+            if (line in tel) {
+                displayTel(line)
+            } else {notFound()}
+        } else {notFound()}
+    }
+}
+
+function displayTel(port) {
+    el('tel-floor').innerText = port.substring(0,1);
+    el('tel-room').innerText = "" + port.substring(0,1) + ('0' + (port.charCodeAt(1)-96)).slice(-2);
+    el('tel-port').innerText = port.charCodeAt(2)-96;
+
+    el('tel-line').innerText = tel[port].line
+
+    el('tel-' + tel[port].dev).style.display = "block";
+    el('tel').style.display = "flex";
+}
+
+function displayEther(port) {
+    el('ether-floor').innerText = port.substring(0,1);
+    el('ether-room').innerText = "" + port.substring(0,1) + ('0' + (port.charCodeAt(1)-96)).slice(-2);
+    el('ether-port').innerText = port.charCodeAt(2)-96;
+    if (ether[port].attrib)
+        el(ether[port].dev).getElementsByClassName('attrib')[0].innerText = ether[port].attrib;
+
+    el('rack-room').innerText = racks[ether[port].loc[0]];
+    el('rack-patch').innerText = ether[port].loc[1];
+    el('rack-port').innerText = ether[port].loc[2];
+
+    el(ether[port].dev).style.display = "block";
+    el('ether').style.display = "flex";
+}
+
+function beforeSearch() {
+    [
+        'notFound',
+        'ether', 'printer', 'computer', 'ap', 'server', 'switch', 
+        'tel', 'tel-phone', 'tel-empty'
+    ].forEach(e => {
+        el(e).removeAttribute('style');
+    });
+}
+
+function notFound() {
+    el('notFound').style.display = "block";
+}
+
+function el(elm) {
+    return document.getElementById(elm)
+}
+
+function getKeyByValue(object, value) {
+    return Object.keys(object).find(key => object[key] === value);
+}
+
+el('search').addEventListener("input", e => {search(e.target.value)})
